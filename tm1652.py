0c9eb4ae4d8afab022bdf3cedaaf3a5d24e76c96
jkorte <joerg.korte@gmail.com>
2025-03-03 20:28:38 +0100

    tm1652.py: docu

--- tm1652.py
+++ tm1652.py
@@ -1,3 +1,29 @@
+"""
+MicroPython TM1652 4x7-segment LED display driver
+https://github.com/jkorte-dev/micropython-tm1652
+
+MIT License
+Copyright (c) 2018 Mike Causer
+Copyright (c) 2025 Joerg Korte
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
 import time
 
 # WeActStudio.DigitalTubeModule (TM1652) micropython example
@@ -50,7 +76,6 @@ class TM1652:
         self.clear()
         self.uart.close()
     
-    # stolen from https://github.com/mcauser/micropython-tm1638/blob/master/tm1638.py    
     def encode_char(self, char):
         """Convert a character 0-9, a-z, space, dash or star to a segment."""
         o = ord(char)
