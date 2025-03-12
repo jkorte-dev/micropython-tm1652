fdd1621ed0b8acf950ee3ff6494de70cf29fa059
jkorte <joerg.korte@gmail.com>
2025-03-12 21:02:51 +0100

    tm1652_example.py: example using gpio tx instead of uart.

--- tm1652_example.py
+++ tm1652_example.py
@@ -1,9 +1,75 @@
-from machine import UART
+from machine import UART, Pin
 import time
 from tm1652 import TM1652, SEGMENTS
 
 
+class FakeUART:
+
+    def __init__(self, sda_pin, delay=None):
+        self.sda_pin = sda_pin
+        sda_pin.init(Pin.OUT)
+        self.delay = delay
+
+    def write(self, buf):
+        for data in buf:
+            self.writechar(data)
+
+    def writechar(self, data):
+        # Send a byte to the chip the way the TM1652 likes it (LSB-first, UART serial 8E1 - 8 bits,
+        # parity bit set to 0 when odd, one stop bit)
+        #  - start bit, 8x data bits, parity bit, stop bit; 52 us = 19200bps
+        t = time.ticks_us()
+        bit_delay = self.delay
+        parity = True
+        r = range(8)
+        gpio = self.sda_pin.value
+        idle = time.sleep_us
+
+        # state = machine.disable_irq() # todo noInterrupts();
+
+        # start - low
+        gpio(0)
+        idle(bit_delay)
+
+        for _ in r:
+            if data & 1:
+                parity = not parity
+            gpio(data & 1)
+            data >>= 1
+            idle(bit_delay)
+
+        # parity - low when odd
+        gpio(int(parity))
+        idle(bit_delay)
+
+        # stop - high
+        gpio(1)
+
+        # machine.enable_irq(state) #todo interrupts();
+        idle(bit_delay)
+
+        # idle - remain high
+        idle(bit_delay)
+        return time.ticks_diff(time.ticks_us(), t)
+
+    def init(self, baudrate=19200, bits=8, parity=None, stop=1, timeout=0):
+        if not self.delay:
+            timing = 1 / baudrate * 1e6  # µs
+            target_t = timing * 12  # 11 waits per byte
+            sample = bytearray([0x0d])
+            for d in range(4, timing):  # delay in µs
+                self.delay = d
+                t = self.writechar(sample[0])  # µs
+                if target_t < t < (target_t + 75):
+                    print(f"calibrated.  {target_t} µs, ∂:{t-target_t} µs, delay:{d} µs")
+                    break
+
+    def close(self):
+        pass  # nothing to do
+
+
 def test_display():
+    # uart = FakeUART(Pin('B1', Pin.OUT)) # stm32f411 using gpio pin B1 instead of uart tx
     # uart = UART(2, 19200) # stm32f411 tx=PA2
     uart = UART(1, 19200)  # esp8266 tx=D4 (gpio2)
 
